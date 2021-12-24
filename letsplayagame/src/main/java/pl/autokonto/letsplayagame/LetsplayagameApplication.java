ce40b0063dcc31e65c9e0a307333b48e42f7a0e4
r.brzezinski <rafalbrzez@gmail.com>
2021-12-24 14:40:02 +0100

    initialization

---
+++ letsplayagame/src/main/java/pl/autokonto/letsplayagame/LetsplayagameApplication.java
@@ -0,0 +1,12 @@
+package pl.autokonto.letsplayagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsplayagameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetsplayagameApplication.class, args);
+    }
+}
---
+++ letsplayagame/src/test/java/pl/autokonto/letsplayagame/LetsplayagameApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.autokonto.letsplayagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsplayagameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

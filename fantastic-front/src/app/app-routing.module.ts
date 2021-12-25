39eaa41ff1dd4e691d1f83128f6ee99d1a3ddd03
r.brzezinski <rafalbrzez@gmail.com>
2021-12-25 13:45:16 +0100

    zadanie 1: utworzenie dwóch kompnentów

--- fantastic-front/src/app/app-routing.module.ts
+++ fantastic-front/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsernameComponent } from './username/username.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'username', component: UsernameComponent },
+  { path: 'user-info', component: UserInfoComponent },
+  { path: '',   redirectTo: '/username', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- fantastic-front/src/app/app.module.ts
+++ fantastic-front/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsernameComponent } from './username/username.component';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsernameComponent,
+    UserInfoComponent
   ],
   imports: [
     BrowserModule,
